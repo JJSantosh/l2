87e9e48977df140591bed3995845b11b8f94d89c
JJSantosh <jairaj.hp@gmail.com>
2024-12-26 18:05:02 +0530

    Add files via upload

--- Act 4.py
+++ Act 4.py
@@ -1,17 +1,7 @@
-print("Hey,This is santosh \n ")
-print("Birthdays of my friends are\n")
+a = ['Hello', 'world', 'from', 'python']
+res = ' '.join(a)
+print(res)
 
-Billy1="31st Dec"
-print("Billy's birthday is on",Billy1)
-
-Jimmy2="29th Feb"
-print("Jimmy's birthday is on",Jimmy2)
-
-Timmy3="1st Jan"
-print("Timmy's birthday is on",Timmy3)
-
-Chandler4="18th Oct"
-print("Chandler's birthday is on",Chandler4)
-
-Logan5="4th Oct"
-print("Logan's birthday is on",Logan5)
+x="Santosh"
+print(str.upper(x))
+print(str.lower(x))
